045a7b7459b5382b27a0382d629d2a219e1aff02
gahyeonn <parka0017@gmail.com>
2022-04-04 11:44:27 +0900

    @Api 이용해 tag 추가

--- src/main/java/com/gahyeonn/swagger/controller/ApiController.java
+++ src/main/java/com/gahyeonn/swagger/controller/ApiController.java
@@ -1,9 +1,17 @@
 package com.gahyeonn.swagger.controller;
 
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "swagger 사용 Controller")
 @RestController
 @RequestMapping("/api")
 public class ApiController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
 }
